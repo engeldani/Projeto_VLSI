4963a2499d1e4c224d9c3c47cbfa43685b80c1dc
Adriano Jung <adrianoluis.jungf@gmail.com>
2022-04-22 21:23:46 -0300

    input - saídas

---
+++ src/fetch.vhd
@@ -0,0 +1,19 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_unsigned.all;
+
+entity fetch is
+    port (
+        --Entrada
+        data_memory : in std_logic_vector(7 downto 0);
+        
+        --Saida
+        address_emmory : out std_logic_vector(11 downto 0);
+        instruction : out std_logic_vector(15 downto 0)
+    );
+end fetch;
+
+architecture fetch of fetch is
+begin
+   
+end architecture;
