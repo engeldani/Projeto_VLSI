ac78d8fa6f6870e8c929aeaa53a126237a5748c9
Adriano Jung <adrianoluis.jungf@gmail.com>
2022-04-22 21:58:53 -0300

    correção

--- src/fetch.vhd
+++ src/fetch.vhd
@@ -8,12 +8,15 @@ entity fetch is
         data_memory : in std_logic_vector(7 downto 0);
         program_counter : in std_logic_vector(11 downto 0);
         --Saida
-        address_emmory : out std_logic_vector(11 downto 0);
+        address_memory : out std_logic_vector(11 downto 0);
         instruction : out std_logic_vector(15 downto 0)
     );
 end fetch;
 
+
+
+
 architecture fetch of fetch is
 begin
-   
+    
 end architecture;
