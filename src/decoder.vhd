3ff60308c1c42f25b764aee20a0c8f0370055044
engeldani <d.engel@hotmail.com>
2022-04-29 19:30:38 -0300

    Update decoder.vhd

--- src/decoder.vhd
+++ src/decoder.vhd
@@ -1,12 +1,12 @@
 library ieee;
 use ieee.std_logic_1164.all;
  
-entity decoder is
+entity chip8 is
 port(
 op_code : in std_logic_vector(15 downto 0);
  : out std_logic_vector(3 downto 0)
 );
-end decoder;
+end chip8;
     architecture decodificador of chip8 is
         
         constant OPCODES : std_logic_vector(7 downto 0) := X"0C";
