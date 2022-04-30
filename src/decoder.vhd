446c77f1ea4f45369cdec59cddac525685aca5ae
engeldani <d.engel@hotmail.com>
2022-04-29 21:28:38 -0300

    Update decoder.vhd

--- src/decoder.vhd
+++ src/decoder.vhd
@@ -14,20 +14,20 @@ port(
 end chip8;
     architecture decodificador of chip8 is
         
-        constant OPCODES : std_logic_vector(7 downto 0) := X"0C";
+        constant OPCODES               : std_logic_vector(7 downto 0) := X"0C";
      
         signal VX_LOAD, VX_VAL, VY_VAL : std_logic_vector(7 downto 0);
-        signal VX_ADDR, VY_ADDR : natural range 0 to 15;
-        signal VX_SEL : natural range 0 to 3;
-        signal VX_LOAD_SEL : natural range 0 to 7;
-        signal VX_EN : std_logic;
+        signal VX_ADDR, VY_ADDR        : std_logic_vector(15 downto 0);
+        signal VX_SEL                  : std_logic_vector(3 downto 0);
+        signal VX_LOAD_SEL             : std_logic_vector(7 downto 0);
+        signal VX_EN                   : std_logic;
         
-        signal IR_VAL : std_logic_vector(7 downto 0);
-        signal IR_EN : std_logic;
+        signal IR_VAL                  : std_logic_vector(7 downto 0);
+        signal IR_EN                   : std_logic;
         
-        signal I_LOAD, I_VAL : std_logic_vector(11 downto 0);
-        signal I_SEL : natural range 0 to 2;
-        signal I_EN : std_logic;
+        signal I_LOAD, I_VAL           : std_logic_vector(11 downto 0);
+        signal I_SEL                   : std_logic_vector(2 downto 0);
+        signal I_EN                    : std_logic;
 begin
 
    -- Máquina de estados
