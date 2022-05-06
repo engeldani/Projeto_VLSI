71ee99278ea264200d037e20d1834da1ddd6612c
engeldani <d.engel@hotmail.com>
2022-05-06 20:58:04 -0300

    Update decoder.vhd

--- src/decoder.vhd
+++ src/decoder.vhd
@@ -31,31 +31,6 @@ end chip8;
         signal I_SEL                   : std_logic_vector(2 downto 0);
         signal I_EN                    : std_logic;
 begin
-   entity c8_60hz is
-      port( CLK      : in std_logic;
-            TICK     : out std_logic)
-   end c8_60hz;
-   
-   architecture rtl of c8_60hz is
-      -- FPGA clock is 50 MHz.
-      -- 50 MHz / 60 Hz = 833333.3
-   
-      constant MAX : natural := 833333 - 1;
-      signal COUNTER : natural range 0 to MAX;
-   begin
-      TICK <= '1' when COUNTER = MAX else '0';
-      
-      process(CLK)
-      begin
-         if rising_edge(CLK) then
-            COUNTER <= COUNTER + 1;
-            
-            if COUNTER = MAX then
-               COUNTER <= 0;
-            end if;
-         end if;
-      end process;
-   end rtl;
 
    -- Máquina de estados
     process(ESTADO_ATUAL, INICIAR, PROG_FLAG, ADDR_END)
