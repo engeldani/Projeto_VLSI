28d685977dd7473913e9625b18e961157c797617
Angelo Elias Dalzotto <angelodalzotto97@gmail.com>
2022-05-20 20:51:03 -0300

    Atualizado esqueleto teclado

--- src/teclado.vhd
+++ src/teclado.vhd
@@ -1,61 +1,93 @@
+--     A B C D
+-- E-1-|-|-|-|-
+-- F-0-|-+-|-|-
+-- G-1-|-|-|-|-
+-- H-1-|-|-|-|-
+--     R R R R
+--     0 0 0 0
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 
 ENTITY teclado IS
     PORT (
-        --add reset
         CLOCK : IN STD_LOGIC;
-        A, B,C, D : IN STD_LOGIC;
+        reset : in STD_LOGIC;
+
+        A, B, C, D : IN STD_LOGIC;
         E, F, G, H : OUT STD_LOGIC;
-        TECLA : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+        TECLA_OUT : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
     );
 END teclado;
 --
-ARCHITECTURE teclado OF teclado IS
-    SIGNAL SEQUENCIA : STD_LOGIC_VECTOR(3 DOWNTO 0); -- Registrador usado para guardar os valores das colunas
-    TYPE estados IS (ATIVA_COLUNA, ATIVA_LINHA); -- criação do tipo para a maquina de estados
-    SIGNAL teclado : estados := ATIVA_COLUNA; -- Declaração da maquina de estado e definição do estado inicial.
+
+ARCHITECTURE rtl OF teclado IS
+    TYPE estados IS (ESPERA_COLUNAS, ATIVA_E, ATIVA_F, ATIVA_G, ATIVA_H); -- criação do tipo para a maquina de estados
+    
+    SIGNAL estado_ativo : estados := ESPERA_COLUNAS; -- Declaração da maquina de estado e definição do estado inicial.
+    SIGNAL estado_proximo : estados := ESPERA_COLUNAS;
+
+    signal col_state : std_logic_vector(3 downto 0);
+    signal col_prev  : std_logic_vector(3 downto 0);
+    signal tecla  : -- Escolher representar como 16 bits (1 bit por tecla) ou 4 bits (formato hexa, numerico);
 BEGIN
-    PROCESS (A,B,C,D,E,F,G,H)--Fazer em forma de flipflop
-        CASE teclado IS
-                -- Caso o estado seja ATIVA_COLUNA, grava as colunas em SEQUENCIA e muda o estado
-            WHEN ATIVA_COLUNA =>
-                IF (A = '1' OR B = '1' OR C = '1' OR D ='1') THEN
-                    SEQUENCIA (3 DOWNTO 0) <= A and B and C and D;
-                    teclado <= ATIVA_LINHA;
-                END IF;
-            WHEN OTHERS => -- (ou seja, quando "ATIVA_LINHA")
-                -- Verifica se o botão foi solto, se não continua em ATIVA_LINHA
-                IF E = '0' AND F = '0' AND G = '0' AND H = '0' THEN
-                    teclado <= ATIVA_COLUNA;
-                END IF;
+
+    -- COMB?
+
+    -- SINCRONO
+    process (clock, reset)
+        if reset = '1' then
+            E <= '1';
+            F <= '1';
+            G <= '1';
+            H <= '1';
+            estado_ativo <= ESPERA_COLUNAS;
+            col_state <= '0000';
+        elsif clock'event and clock = '1' then
+            -- Sincronizando a entrada
+            col_state <= A and B and C and D;
+
+            estado_ativo <= estado_proximo;
+            if estado_proximo = ESPERA_COLUNAS then
+                col_prev <= col_state;
+            elsif estado_proximo = ESPERA_COLUNAS;
+                tecla_out <= tecla;
+            end if;
+        end if;
+    end process;
+
+    -- TRANSICAO
+    PROCESS (estado_ativo, col_state)--Fazer em forma de flipflop
+        CASE estado_ativo IS
+            -- Caso o estado seja ATIVA_COLUNA, grava as colunas em SEQUENCIA e muda o estado
+            WHEN ESPERA_COLUNAS =>
+                IF (col_state(3) = '1' OR col_state(2) = '1' OR col_state(1) = '1' OR col_state(0) ='1') THEN
+                    -- Alguma das colunas teve clique!
+                    estado_proximo <= ATIVA_E;
+
+                    -- Salvar o col_state no col_prev
+                else 
+                    estado_proximo <= ESPERA_COLUNAS;
+                end if;
+            WHEN ATIVA_E =>
+                -- fazer
+                -- Verificar se nao é o E mesmo
+                -- Se for, encontramos a tecla, e vamos esperar as colunas
+
+                -- Se nao, proxima linha
+                estado_proximo <= ATIVA_F;
+            WHEN ATIVA_F =>
+                if(col_prev(3) = '1' and col_state(3) = '0') then
+                    tecla = x"04";
+                    estado_proximo <= ESPERA_COLUNAS;
+                elsif(col_prev(2) = '1' and col_state(2) = '0') then
+                    tecla = x"05";
+                    estado_proximo <= ESPERA_COLUNAS;
+                else
+                    estado_proximo <= ATIVA_G;
+                end if;
+            -- fazer G
+            -- fazer H
         END CASE;
     END PROCESS;
-    PROCESS (teclado)
-        -- Nesse processo serão selecionados F, E, D, K, J, H, G como entrada ou saída
-        CASE teclado IS
-
-                -- Caso o estado seja ATIVA_COLUNA, seleciona F, E, D como entrada e K, H, J, G como saídas
-            WHEN ATIVA_COLUNA =>
-                A <='1';
-                B <='1';
-                C <='1';
-                D <='1';
-                E <='0';
-                F <='0';
-                G <='0';
-                H <='0';
-
-                -- Caso o estado seja ATIVA_LINHA, seleciona F, E, D, como saida e K, H, J, G como entradas
-            WHEN OTHERS =>
-                A <='0';
-                B <='0';
-                C <='0';
-                D <='0';
-                E <='1';
-                F <='1';
-                G <='1';
-                H <='1';
-
-        END CASE;
-    END PROCESS;
+end architecture;
