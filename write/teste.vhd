dccd8edf0c042829a992853d1eb5e328dced6232
AnthonyVanzan <53534250+AnthonyVanzan@users.noreply.github.com>
2022-07-07 18:52:47 -0300

    Update teste.vhd

--- write/teste.vhd
+++ write/teste.vhd
@@ -16,7 +16,7 @@ entity draw is
             fb_write      : out std_logic; --só um pulso pra realizar a escrita?  fp 64 x 32 bits 256 posições -- write controla a escrita
             fb_addr       : out std_logic_vector(7 downto 0); --veirificar se é a posição do objeto na matriz -8 bits
             fb_data       : out std_logic_vector(7 downto 0);
-            busy          : out std_logic; --enquanto ta realizando fica em 1 pra não receber mais instruções 
+            busy          : out std_logic --enquanto ta realizando fica em 1 pra não receber mais instruções 
     );
 end draw;
 
@@ -24,7 +24,7 @@ architecture draw of draw is
 
 begin
 --maquina de estados (inicial, ) cls 2 e drw 3 
-	type SM_Main_t is (start, cls1, cls2, wrt1,wrt2, wrt3);
+	type SM_Main_t is (start, cls1, cls2, wrt1, wrt2, wrt3);
   signal SM_Main_r : SM_Main_t := Idle_s;
 	process(clock) 
         begin
